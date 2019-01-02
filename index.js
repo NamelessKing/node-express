6a34d5c63b89a601e689f1477be053ac9ff4ca84
namelessking <habibur.tanjin@gmail.com>
2019-01-02 02:14:31 +0100

    Express serve static files

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));//using morgan in development for printing out additional information 
+
+app.use(express.static(`${__dirname}/public`));//This tells Express to serv static files
 
 app.use((req, res, next) => {
-    console.log(req.header);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
-    res.end('<html><body><h1>This is an Express server</h1></body></html >');
+    res.end(`<html><body><h1>This is an Express server </h1></body></html >`);
 });
 
 const server = http.createServer(app);

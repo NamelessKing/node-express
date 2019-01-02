a88aa110e25dc0dc32e149249e917fb33d0c9dca
namelessking <habibur.tanjin@gmail.com>
2019-01-02 01:51:41 +0100

    created express server

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const port = 3000;
 
 const app = express();
 
-app.use((req, res, next) => { 
-     
-})
+app.use((req, res, next) => {
+    console.log(req.header);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express server</h1></body></html >');
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});

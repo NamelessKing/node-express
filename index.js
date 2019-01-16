e3ddd5e1781bcb0751d7dea6bd359c0a714350aa
namelessking <habibur.tanjin@gmail.com>
2019-01-16 01:57:07 +0100

    creating routes

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));//using morgan in development for printing out additional information 
+app.use(bodyParser.json());//allows to parse the body of the request message which is formatted in JSON
+
+app.all('/dishes', (req, res, next) => { 
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+});
+
+app.get('/dishes', (req, res, next) => {
+    res.end(`Will send all the dishes`);
+});
+
+app.post('/dishes', (req, res, next) => {
+    res.end(`Will add the dish: ${req.body.name} with details: ${req.body.description}`);
+});
+
+app.put('/dishes', (req, res, next) => {
+    res.statusCode = 403;
+    res.end(`PUT operation not supported on /dishes`);
+});
+
+app.delete('/dishes', (req, res, next) => {
+    res.end(`Deleting all the dishes`);
+});
+
+
 
 app.use(express.static(`${__dirname}/public`));//This tells Express to serv static files
 
